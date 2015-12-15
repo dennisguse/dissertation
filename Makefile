c34ae8dd2ef1f1fae12aed63fdc7a5aa205ff535
Dennis Guse <dennis.guse@alumni.tu-berlin.de>
2015-12-15 18:00:41 +0100

    Stuff.

--- Makefile
+++ Makefile
@@ -8,7 +8,7 @@ tex: 00main.tex chapter07.tex chapter08.tex chapter09.tex appendix.tex
 	echo "Done"
 
 %.tex : %.Rnw
-	Rscript -e "library(knitr); knit('$<')" || rm $@
+	Rscript -e "library(knitr); knit('$<')" || (rm $@ && exit -1)
 
 clean:
 	-rm *.aux
--- R_eval.R
+++ R_eval.R
@@ -12,7 +12,7 @@ participants <- function(experiment_filter, condition_filter=unique(timeseries$c
 participants("E1", 3)
 
 mos_qu_with_sd_by_condition <- function(experiment_filter, performance_level_filter=unique(timeseries$performance_level), condition_filter=unique(timeseries$condition), id_filter=unique(timeseries$id)) {
- d = subset(timeseries$QU, experiment==experiment_filter & performance_level %in% performance_level_filter & condition %in% condition_filter & id %in% id_filter)
+ d = subset(timeseries$QU, experiment %in% experiment_filter & performance_level %in% performance_level_filter & condition %in% condition_filter & id %in% id_filter)
  
  mean_sd(d)
 }
@@ -20,7 +20,7 @@ mos_qu_with_sd_by_condition("E1", "HP", 3)
 mos_qu_with_sd_by_condition("E1", "HP", c(1:3), 1)
 
 mos_iqu_with_sd_by_condition <- function(experiment_filter, condition_filter=unique(timeseries$condition), id_filter=unique(timeseries$id), service_filter=unique(timeseries$service)) {
- d = subset(timeseries$IQU, experiment==experiment_filter & condition %in% condition_filter & id %in% id_filter & service %in% service_filter)
+ d = subset(timeseries$IQU, experiment %in% experiment_filter & condition %in% condition_filter & id %in% id_filter & service %in% service_filter)
  
  mean_sd(d)
 }
@@ -37,47 +37,65 @@ mos_iqu_with_sd_by_condition("E2b", "4", 6, "video")
 #####STATISTICS!
 
 kruskal <- function(formula=QU~condition, experiment_filter, performance_level_filter=unique(timeseries$performance_level), condition_filter=unique(timeseries$condition), id_filter=unique(timeseries$id)) {
- d = subset(timeseries, experiment==experiment_filter & performance_level %in% performance_level_filter & condition %in% condition_filter & id %in% id_filter)
+ d = subset(timeseries, experiment %in% experiment_filter & performance_level %in% performance_level_filter & condition %in% condition_filter & id %in% id_filter)
 
  r = kruskal.test(formula, d)
  if (r$p.value < 0.0001) paste0("$H(", r$parameter, ")=", round(r$statistic, 4), "$, $p<0.0001$") 
  else paste0("$H(", r$parameter, ")=", round(r$statistic, 4), "$, $p=", round(r$p.value, 4), "$") 
 }
 kruskal(QU~condition, "E1", "HP", unique(timeseries$condition), c(1:3))
+kruskal(QU~condition, "E2b", "HP", c("5a0", "5a4"), c(1:3))
 
-wilcox <- function(formula=QU~condition, experiment_filter, performance_level_filter=unique(timeseries$performance_level), condition_filter=unique(timeseries$condition), id_filter=unique(timeseries$id), alternative="two.sided", paired=F) {
- d = subset(timeseries, experiment==experiment_filter & performance_level %in% performance_level_filter & condition %in% condition_filter & id %in% id_filter)
+
+
+
+wilcox <- function(formula=QU~condition, experiment_filter, performance_level_filter=unique(timeseries$performance_level), condition_filter=unique(timeseries$condition), id_filter=unique(timeseries$id), alternative="two.sided", paired=F, diffOnly = F) {
+ d = subset(timeseries, experiment %in% experiment_filter & performance_level %in% performance_level_filter & condition %in% condition_filter & id %in% id_filter)
 
  r = wilcox.test(formula, d, exact = F, conf.int = T, alternative = alternative, paired = paired)
- if (r$alternative == "two.sided") r$alternative = ""
- else r$alternative = ", one-sided"
+ if (diffOnly == T) {
+  paste0("$\\triangle=", format(round(r$estimate, 2), nsmall=2), "$")
+ } else {
+  if (r$alternative == "two.sided") r$alternative = ""
+  else r$alternative = ", one-sided"
   
- if (r$p.value < 0.0001) paste0("$W=", round(r$statistic, 4), "$, $p<0.0001$, $\triangle=", format(round(r$estimate, 2), nsmall=2), "$", r$alternative) 
- else paste0("$W=", round(r$statistic, 4), "$, $p=", round(r$p.value, 4), "$, $\triangle=", format(round(r$estimate, 2), nsmall=2), "$", r$alternative) 
+  if (r$p.value < 0.0001) paste0("$W=", round(r$statistic, 4), "$, $p<0.0001$, $\\triangle=", format(round(r$estimate, 2), nsmall=2), "$", r$alternative) 
+  else paste0("$W=", round(r$statistic, 4), "$, $p=", round(r$p.value, 4), "$, $\\triangle=", format(round(r$estimate, 2), nsmall=2), "$", r$alternative) 
+ }
 }
 wilcox(QU~performance, "E1",  c("HP", "LP"))
 wilcox(QU~performance, "E1",  c("HP", "LP"), alternative="greater")
 wilcox(QU~performance, "E1",  c("HP", "LP"), alternative="less")
+wilcox(QU~condition, "E1", "HP", c("4", "7"), c(1:3), diffOnly=T)
 
-pairwise.wilcox.test(timeseries_by_study_H1_TEL_ONE$IQU, timeseries_by_study_H1_TEL_ONE$condition, exact=F, alternative = "less") 
-
-wilcox.pairwise <- function(formula=IQU~condition, experiment_filter, performance_level_filter=unique(timeseries$performance_level), condition_filter=unique(timeseries$condition), id_filter=unique(timeseries$id)) { #, alternative="two.sided", paired=F, p.adjust.method = "holm"
+wilcox.pairwise.table <- function(formula=IQU~condition, experiment_filter, performance_level_filter=unique(timeseries$performance_level), condition_filter=unique(timeseries$condition), id_filter=unique(timeseries$id)) { #, alternative="two.sided", paired=F, p.adjust.method = "holm"
  d = subset(timeseries, experiment==experiment_filter & performance_level %in% performance_level_filter & condition %in% condition_filter & id %in% id_filter)
  
  f = all.names(formula, functions=F)
  r = pairwise.wilcox.test(d[[f[1]]], d[[f[2]]], exact=F) 
- r$p.value = format(round(r$p.value, 2), nsmall=2)
- r$p.value[r$p.value == "  NA"] = "-"
+ p = format(round(r$p.value, 2), nsmall=2)
+ p[p == "  NA"] = "-"
 
- print(sprintf("%s\\ \n", paste(colnames(r$p.value), collapse="&")))
- value=apply(r$p.value, 1, function(x){ sprintf("%s\\ \n", paste(x, collapse="&"))})
+ print(sprintf("%s\\ \n", paste(colnames(p), collapse="&")))
+ value=apply(p, 1, function(x){ sprintf("%s\\ \n", paste(x, collapse="&"))})
  for(row in 1:length(value)) {
   print(paste(names(value)[row], value[row], sep="&"))
  }
  
  return (r)
 }
-r=wilcox.pairwise(experiment_filter = "E2a")
+wilcox.pairwise.print <- function(formula=IQU~condition, experiment_filter, performance_level_filter=unique(timeseries$performance_level), condition_filter=unique(timeseries$condition), id_filter=unique(timeseries$id)) { #, alternative="two.sided", paired=F, p.adjust.method = "holm"
+ d = subset(timeseries, experiment==experiment_filter & performance_level %in% performance_level_filter & condition %in% condition_filter & id %in% id_filter)
+ 
+ f = all.names(formula, functions=F)
+ r = pairwise.wilcox.test(d[[f[1]]], d[[f[2]]], exact=F) 
+}
+
+
+wilcox.pairwise.print(QU~condition, "E1", "HP", unique(timeseries[["condition"]]), c(1:3))$p.value[7, 4]
+wilcox.pairwise.print(QU~condition, "E1", "HP", unique(timeseries[["condition"]]), c(1:3))$p.value
+wilcox.pairwise.print(QU~condition, "E2a", "HP", unique(timeseries[["condition"]]), c(1:3))[["p.value"]][5,4]
+
 
 
 
