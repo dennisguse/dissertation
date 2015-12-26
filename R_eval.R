12f0f3b5610a1a1a28b7534c4a72c8fc5072fc23
Dennis Guse <dennis.guse@alumni.tu-berlin.de>
2015-12-26 13:12:51 +0100

    R_eval: wilcox.pairwise has now formatting AND estimate.

--- R_eval.R
+++ R_eval.R
@@ -90,15 +90,43 @@ wilcox.pairwise.print <- function(formula=IQU~condition, experiment_filter, perf
  d = subset(timeseries, experiment==experiment_filter & performance_level %in% performance_level_filter & condition %in% condition_filter & id %in% id_filter)
  
  f = all.names(formula, functions=F)
- r = pairwise.wilcox.test(d[[f[1]]], d[[f[2]]], exact=F, alternative = alternative, paired = paired) 
+ p = pairwise.wilcox.test(d[[f[1]]], d[[f[2]]], exact=F, alternative = alternative, paired = paired)$p.value
+ estimate = pairwise.wilcox.estimate(d[[f[1]]], d[[f[2]]], exact=F, alternative = alternative, paired = paired)$p.value
+ 
+ result = p
+ for(x in 1:nrow(p)) {
+  for(y in 1:ncol(p)) {
+   if (is.na(p[x, y])) result[x, y] = "NA"
+   else 
+   if (p[x, y] > 0.05) result[x, y] = sprintf("$p=%.2f$", p[x, y])
+   else if (p[x, y] < 0.001) result[x, y] = sprintf("$p<0.001$, $\\triangle=%.2f$", estimate[x, y])
+   else result[x, y] = sprintf("$p=%.2f$, $\\triangle=%.2f$", p[x, y], estimate[x, y])
+   }
+  }
+ return (result)
+ #result = as.data.frame(outer(r, estimate, function(x, y) {return (paste(x, ": ", y))} ))
+ 
 }
+pairwise.wilcox.estimate <-function (x, g, p.adjust.method = p.adjust.methods, paired = FALSE, ...) {
+ p.adjust.method <- match.arg(p.adjust.method)
+ DNAME <- paste(deparse(substitute(x)), "and", deparse(substitute(g)))
+ g <- factor(g)
+ METHOD <- if (paired) 
+  "Wilcoxon signed rank test"
+ else "Wilcoxon rank sum test"
+ compare.levels <- function(i, j) {
+  xi <- x[as.integer(g) == i]
+  xj <- x[as.integer(g) == j]
+  wilcox.test(xi, xj, paired = paired, conf.int = T,...)$estimate #This is the original implementation of pairwise.wilcox.tet, but instead $p.value here $estimate is used!
+ }
+ PVAL <- pairwise.table(compare.levels, levels(g), "none")
+ ans <- list(method = METHOD, data.name = DNAME, p.value = PVAL, 
+             p.adjust.method = p.adjust.method)
+ class(ans) <- "pairwise.htest"
+ ans
+}
+wilcox.pairwise.print(QU~condition, "E1", "HP", unique(timeseries[["condition"]]), c(1:3))
+wilcox.pairwise.print(QU~condition, "E1", "HP", unique(timeseries[["condition"]]), c(1:3))[7, 4]
+pairwise.wilcox.estimate(d[[f[1]]], d[[f[2]]], exact=F, alternative = "two.sided", paired = F)
 
-
-wilcox.pairwise.print(QU~condition, "E1", "HP", unique(timeseries[["condition"]]), c(1:3))$p.value[7, 4]
-wilcox.pairwise.print(QU~condition, "E1", "HP", unique(timeseries[["condition"]]), c(1:3))$p.value
-wilcox.pairwise.print(QU~condition, "E2a", "HP", unique(timeseries[["condition"]]), c(1:3))[["p.value"]][5,4]
-
-
-
-
-
+wilcox(QU~condition, "E1", "HP", c("7", "4"), c(1:3))
